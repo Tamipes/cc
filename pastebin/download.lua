c2fb542ce69b2f70c2aca0db3f4a8b28b9c8d3cb
System administrator <tamipesagniyah@gmail.com>
2025-04-04 21:15:26 +0200

    chore: clean up pastebin download script

--- pastebin/download.lua
+++ pastebin/download.lua
@@ -1,21 +1,24 @@
 ---Get the file name and its extension from the path.
 ---@param _path string
 ---@return string
-function getFileInPath(_path)
-    while string.find(_path, "/") do
-        _path = string.sub(_path, string.find(_path, "/") + 1)
-    end
-    return _path
+function GetFileInPath(_path)
+  while string.find(_path, "/") do
+    _path = string.sub(_path, string.find(_path, "/") + 1)
+  end
+  return _path
 end
-function getFolderPath(_path)
-    return string.sub(_path,1,#_path-#getFileInPath(_path))
+
+function GetFolderPath(_path)
+  return string.sub(_path, 1, #_path - #GetFileInPath(_path))
 end
-function downloadComplete()
-    downloaded = true
-    if fs.exists(".uninstall") then
-        file = fs.open(".uninstall","r")
-        programs = textutils.unserialise(file.readAll())
-    end
+
+function DownloadComplete()
+  Downloaded = true
+  if fs.exists(".uninstall") then
+    local file = fs.open(".uninstall", "r")
+    ---@diagnostic disable-next-line: param-type-mismatch, need-check-nil
+    Programs = textutils.unserialise(file.readAll())
+  end
 end
 
 ---Trys to download a file from a given path, uses multiple links.
@@ -25,57 +28,62 @@ end
 ---@param _fileName string
 ---@param _url string
 ---@return boolean
-function downloadFile(_path, _override, _fileName,_url)
-    url = string.format(_url, _path)
-    if _fileName == nil then _fileName = getFileInPath(_path) end
-    request ,err = http.get(url)
-    if request ~= nil and (request.getResponseCode() == 200) then
-        if _override and fs.exists(_fileName) then shell.run("delete", _fileName) end
-        file = fs.open(_fileName, "w")
-        file.write(request.readAll())
-        file.close()
+function DownloadFile(_path, _override, _fileName, _url)
+  Url = string.format(_url, _path)
+  if _fileName == nil then _fileName = GetFileInPath(_path) end
+  Request, err = http.get(Url)
+  if Request ~= nil and (Request.getResponseCode() == 200) then
+    if _override and fs.exists(_fileName) then shell.run("delete", _fileName) end
+    local file = fs.open(_fileName, "w")
+    ---@diagnostic disable-next-line: need-check-nil
+    file.write(Request.readAll())
+    ---@diagnostic disable-next-line: need-check-nil
+    file.close()
 
-        --What to do if download succeded.
-        if doLog then print('Downloaded: '.._fileName) end
-        downloadComplete()
-        return true
-    else
-        print(err)
-    end
-    return false
+    --What to do if download succeded.
+    if DoLog then print('Downloaded: ' .. _fileName) end
+    DownloadComplete()
+    return true
+  else
+    print(err)
+  end
+  return false
 end
 
-
-
-input = {...}
-if input[2] ~=nil then
-    override = string.find( input[2],"true")
+Input = { ... }
+if Input[2] ~= nil then
+  local override = string.find(Input[2], "true")
+  if override then
+    Override = true
+  else
+    Override = false
+  end
 end
 
-doLog = settings.get("logDownloads", true)
+DoLog = settings.get("logDownloads", true)
 
-downloaded = false
+Downloaded = false
 
-urls = {"https://computercraft.zomzi.moe/download/%s"}
-link = settings.get("http_optimal_link", -1)
+Urls = { "https://computercraft.zomzi.moe/download/%s" }
+Link = settings.get("http_optimal_link", -1)
 
-if not (link == -1)then
-    downloadFile(input[1],override,input[3],link)
+if not (Link == -1) then
+  DownloadFile(Input[1], Override, Input[3], Link)
 else
-    for i = 1, #urls do
-        if (downloadFile(input[1],override,input[3],urls[i])) then
-            settings.set("http_optimal_link",urls[i])
-            settings.save(".settings")
-            break
-        end
+  for i = 1, #Urls do
+    if (DownloadFile(Input[1], Override, Input[3], Urls[i])) then
+      settings.set("http_optimal_link", Urls[i])
+      settings.save(".settings")
+      break
     end
+  end
 end
 
 
 --Self update
-if not(downloaded) then
-    shell.run("pastebin","get","QzYCbZqL","Tami/temp")
-    shell.run("delete","Tami/download.lua")
-    shell.run("rename","Tami/temp","Tami/download.lua")
-    shell.run("Tami/download.lua",input[1],input[2],input[3],input[4])
+if not (Downloaded) then
+  shell.run("pastebin", "get", "QzYCbZqL", "Tami/temp")
+  shell.run("delete", "Tami/download.lua")
+  shell.run("rename", "Tami/temp", "Tami/download.lua")
+  shell.run("Tami/download.lua", Input[1], Input[2], Input[3], Input[4])
 end
